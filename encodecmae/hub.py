a755a9e0d1d3606215cab53865fb27161d6ff049
Leonardo Pepino <leonardodpepino@gmail.com>
2024-06-09 17:45:07 -0300

    Avoid problems when using inside gin

--- encodecmae/hub.py
+++ encodecmae/hub.py
@@ -27,7 +27,11 @@ def get_model(model, processor):
 def load_model(model, mode='eval',device='cuda:0'):
     #Get model files
     config_str = gin.config_str()
+    registers = copy.deepcopy(gin.config._REGISTRY)
     gin.clear_config()
+    registry_clear_keys = [k for k,v in gin.config._REGISTRY.items() if k not in ['gin.macro', 'gin.constant', 'gin.singleton', 'ginpipe.core.execute_pipeline', 'encodecmae.hub.get_model']]
+    for k in registry_clear_keys:
+        gin.config._REGISTRY.pop(k)
     available_models = get_available_models()
     if model in available_models:
         model_file = hf_hub_download(repo_id='lpepino/encodecmae-v2', filename='{}.pt'.format(model))
@@ -44,6 +48,8 @@ def load_model(model, mode='eval',device='cuda:0'):
     processor = processor()
     model.load_state_dict(model_state['state_dict'])
     gin.clear_config()
+    gin.config._REGISTRY.clear()
+    gin.config._REGISTRY = registers
     gin.parse_config(config_str)
     if mode=='eval':
         model.eval()
